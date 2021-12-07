3cb4189c4cb7d4d67a5e553b52d74b2b23c25fa0
Daniil <kiselyovda@ya.ru>
2021-12-07 15:39:52 +0300

    end

--- pythonbasic/module_4/index.py
+++ pythonbasic/module_4/index.py
@@ -27,11 +27,33 @@ for index, user in enumerate(users):
                     max_salary = friend['job']['salary']
                     best_occupation['occupation'] = friend['job']['occupation']
                     best_occupation['salary'] = friend['job']['salary']
-            if friend.get('cars'):
-                if friend.get('flights'):
-                    avg_flights = round(len(friend['cars']) / len(friend['flights']), 5)
-            if friend.get('flights'):
-                for flight in friend['flights']:
-                    if flight['country'] in countries:
-                        pass
-print(best_occupation)
+
+avg_flights = 0
+friends_with_cars = 0
+total_flights = 0
+for user in users:
+    friends = user.get('friends', [])
+    for friend in friends:
+        cars = friend.get('cars', None)
+        if cars:
+            friends_with_cars += 1
+            total_flights += len(friend.get('flights', []))
+avg_flights = round(total_flights / friends_with_cars, 5) if friends_with_cars else 0
+
+i = 0
+while i < len(users):
+    need_remove = False
+    friends = users[i].get('friends', [])
+    for friend in friends:
+        flights = friend.get('flights', [])
+        for flight in flights:
+            if flight['country'] in countries:
+                need_remove = True
+                break
+        if need_remove:
+            break
+
+    if need_remove:
+        del users[i]
+    else:
+        i += 1
