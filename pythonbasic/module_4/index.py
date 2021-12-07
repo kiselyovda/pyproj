b28a53b3352774383f7bb71936214e9e645f03f3
Daniil <kiselyovda@ya.ru>
2021-12-07 15:33:19 +0300

    vip_user

--- pythonbasic/module_4/index.py
+++ pythonbasic/module_4/index.py
@@ -8,20 +8,25 @@ users_wrong_password = []
 girls_drivers = []
 best_occupation = {}
 max_salary = 0
+max_friends_total_salary = 0
 vip_user = ''
 for index, user in enumerate(users):
     if user['password'].isdigit():
         users_wrong_password.append({'name': user['name'], 'mail': user['mail']})
     if user.get('friends'):
+        friends_total_salary = 0
         for friend in user.get('friends'):
             if friend.get('cars') and friend.get('sex') == 'F':
                 girls_drivers.append(friend.get('name'))
             if friend.get('job'):
+                friends_total_salary += friend['job']['salary']
+                if friends_total_salary > max_friends_total_salary:
+                    max_friends_total_salary = friends_total_salary
+                    vip_user = user['name']
                 if friend['job']['salary'] > max_salary:
                     max_salary = friend['job']['salary']
                     best_occupation['occupation'] = friend['job']['occupation']
                     best_occupation['salary'] = friend['job']['salary']
-                    vip_user = friend['name']
             if friend.get('cars'):
                 if friend.get('flights'):
                     avg_flights = round(len(friend['cars']) / len(friend['flights']), 5)
